bee10252e7b844058d5f660092f497a87f2472e0
Tatu Tamminen <ttamminen@gmail.com>
2017-06-25 20:28:14 +0300

    adds improved navigation

--- js/boot.js
+++ js/boot.js
@@ -1,70 +1,77 @@
 /* global Modernizr, responsiveNav, SliderModule, 
-		  TouchSliderModule, ContactFormModule, NewsModule, JSONP, _ */
+      TouchSliderModule, ContactFormModule, NewsModule, JSONP, _ */
 
 function ready(fn) {
-	"use strict";
-	if (document.addEventListener) {
-		document.addEventListener('DOMContentLoaded', fn);
-	} else {
-	document.attachEvent('onreadystatechange', function() {
-		if (document.readyState === 'interactive')
-			fn();
-		});
-	}
+  "use strict";
+  if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', fn);
+  } else {
+  document.attachEvent('onreadystatechange', function() {
+    if (document.readyState === 'interactive')
+      fn();
+    });
+  }
 }
 
 function boot() {
-	"use strict";
+  "use strict";
 
-	function initNav() {
-		if(typeof responsiveNav === "undefined") {
-			throw new Error("Could not load responsive nav");
-		}
+  function initNav() {
+    if(typeof responsiveNav === "undefined") {
+      throw new Error("Could not load responsive nav");
+    }
 
-		var nav = document.getElementsByClassName('nav-collapse');
-		if(nav.length === 0) 
-		{
-			return;
-		}
+    var nav = document.getElementsByClassName('nav-collapse');
+    if(nav.length === 0) 
+    {
+      return;
+    }
 
+    responsiveNav('.nav-collapse', {
+      label: '☰ Menu'
+    });
 
-		responsiveNav('.nav-collapse', {
-			label: '☰ Menu'
-		});
-	}
+    var currentLocation = location.pathname;
+    var links = document.querySelectorAll('.nav-collapse a');
+    Utils.forEach(links, function (link) {
+      if(link && link.getAttribute('href') === currentLocation) {
+        Utils.addClass(link.parentNode, 'current-link');
+      }
+    })
+  }
 
-	function initContactForm() {
-		ContactFormModule.init();
-	}
+  function initContactForm() {
+    ContactFormModule.init();
+  }
 
-	function initNews() {
-		var socialmediaupdates = '.social-media-updates';
-		if(document.querySelectorAll(socialmediaupdates).length === 0) {
-			return;
-		}
+  function initNews() {
+    var socialmediaupdates = '.social-media-updates';
+    if(document.querySelectorAll(socialmediaupdates).length === 0) {
+      return;
+    }
 
-		JSONP('http://priimavalmennus-socialmedia.azurewebsites.net/api/facebook', function (data) {
-			NewsModule.init(data, socialmediaupdates);
-		});
-	}
+    JSONP('http://priimavalmennus-socialmedia.azurewebsites.net/api/facebook', function (data) {
+      NewsModule.init(data, socialmediaupdates);
+    });
+  }
 
-	function initSlider() {
-		if(Modernizr.touch) {
-			TouchSliderModule.init();
-		}
-		else {
-			SliderModule.init();
-		}
-	}
+  function initSlider() {
+    if(Modernizr.touch) {
+      TouchSliderModule.init();
+    }
+    else {
+      SliderModule.init();
+    }
+  }
 
-	function init() {
-		initNav();
-		initNews();
-		initContactForm();		
-		initSlider();
-	}
+  function init() {
+    initNav();
+    initNews();
+    initContactForm();		
+    initSlider();
+  }
 
-	init();
+  init();
 }
 
 ready(boot);
--- js/utils.js
+++ js/utils.js
@@ -1,46 +1,51 @@
 /* exported Utils */
 
 var Utils = (function () {
-	"use strict";
+  "use strict";
 
-	return {
-		addClass: function (el, className) {
-			if (el.classList) { 
-				el.classList.add(className);
-			}
-			else {
-			  	el.className += ' ' + className;
-			}
-		},
+  return {
+    addClass: function (el, className) {
+      if (el.classList) { 
+        el.classList.add(className);
+      }
+      else {
+          el.className += ' ' + className;
+      }
+    },
 
-		removeClass: function (el, className) {
-			if (el.classList) {
-	  			el.classList.remove(className);
-	  		}
-			else {
-	  			el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-			}
-		},
+    removeClass: function (el, className) {
+      if (el.classList) {
+          el.classList.remove(className);
+        }
+      else {
+          el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+      }
+    },
 
-		getJSON: function (url, successCallback) {
-			var request = new XMLHttpRequest();
-			request.open('GET', url, true);
+    forEach: function (array, fn) {
+      for (var i = 0; i < array.length; i++)
+        fn(array[i], i);
+    },
 
-			request.onload = function() {
-				if (this.status >= 200 && this.status < 400){
-					// Success!
-					var data = JSON.parse(this.response);
-					successCallback(data);
-				} else {
-					console.log("Ajax request failed");
-				}
-			};
+    getJSON: function (url, successCallback) {
+      var request = new XMLHttpRequest();
+      request.open('GET', url, true);
 
-			request.onerror = function() {
-			  	console.log("Ajax request failed");
-			};
+      request.onload = function() {
+        if (this.status >= 200 && this.status < 400){
+          // Success!
+          var data = JSON.parse(this.response);
+          successCallback(data);
+        } else {
+          console.log("Ajax request failed");
+        }
+      };
 
-			request.send();		
-		}
-	};
+      request.onerror = function() {
+          console.log("Ajax request failed");
+      };
+
+      request.send();		
+    }
+  };
 })();
